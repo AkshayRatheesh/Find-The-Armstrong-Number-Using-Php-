7acba3c9c2a76605594eb9cb89bfcd7104383f77
AkshayRatheesh <akshaypathickal@gmail.com>
2022-04-26 21:09:59 +0530

    html data modified 1

--- armstrong.php
+++ armstrong.php
@@ -1,9 +1,25 @@
-<html>  
-<body>  
- <form method="post">  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>armstrong</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+  <div class="container">
+
+  <form method="post">  
  Enter the Number:  
-   <input type="number" name="number">  
+   <input type="number" name="number"> 
+   <br> <br>
    <input type="submit" value="Submit">  
   </form>  
-</body>  
-</html>  
+
+
+  </div>
+</body>
+</html>
