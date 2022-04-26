107d341aa72bdd3d64953a5c20152ae53a4bf4d4
AkshayRatheesh <akshaypathickal@gmail.com>
2022-04-26 20:58:33 +0530

    html data

---
+++ armstrong.php
@@ -0,0 +1,9 @@
+<html>  
+<body>  
+ <form method="post">  
+ Enter the Number:  
+   <input type="number" name="number">  
+   <input type="submit" value="Submit">  
+  </form>  
+</body>  
+</html>  
